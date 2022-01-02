22911127bf857de163eb6c33af665a8258e442ab
Kaori42 <93971368+Kaori42@users.noreply.github.com>
2022-01-02 13:42:10 +0100

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/dvpt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/dvpt.dir/main.c.obj"
+  "dvpt.exe"
+  "dvpt.exe.manifest"
+  "dvpt.pdb"
+  "libdvpt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/dvpt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
